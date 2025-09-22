64caeea03d18f5751c7065f0f4404a67e254bf78
gersonlaparra13@gmail.com <estudiantev@COED.LOCAL>
2025-09-22 14:47:25 -0600

    Modifique unas cosas

--- Crudd proyect.py
+++ Crudd proyect.py
@@ -3,16 +3,22 @@ from tkinter import *
 from tkinter import ttk
 
 baseDeDatos = sqlite3.connect("Productos.db")
-cur = baseDeDatos.cursor()
-cur.execute("""
+cr = baseDeDatos.cr()
+cr.execute("""
 CREATE TABLE IF NOT EXISTS productos (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     nombre TEXT,
     precio REAL,
     stock INTEGER
 )
 """)
 baseDeDatos.commit()
+
+def mostrar_p():
+    t.delete(*t.get_children())
+    for fila in cr.execute("SELECT * FROM productos"):
+        t.insert("", "end", values=fila)
+
 
 
 
