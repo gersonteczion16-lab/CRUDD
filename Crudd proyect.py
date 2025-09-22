139885c22dbab1f757ac647a3633e477f3c3e3f0
gersonlaparra13@gmail.com <estudiantev@COED.LOCAL>
2025-09-22 14:43:19 -0600

    Agregue la funcion de mostrar producto

--- Crudd proyect.py
+++ Crudd proyect.py
@@ -1,1 +1,18 @@
+import sqlite3
+from tkinter import *
+from tkinter import ttk
+
+baseDeDatos = sqlite3.connect("Productos.db")
+cur = baseDeDatos.cursor()
+cur.execute("""
+CREATE TABLE IF NOT EXISTS productos (
+    id INTEGER PRIMARY KEY,
+    nombre TEXT,
+    precio REAL,
+    stock INTEGER
+)
+""")
+baseDeDatos.commit()
+
+
 
